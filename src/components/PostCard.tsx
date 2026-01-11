e7d4d3e2a6013ce60f5b0ba4ee71c44f39fef5e2
Bulnaug <basalay.1997@mail.ru>
2026-01-11 04:48:43 +0100

    update for vercel

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -12,8 +12,6 @@ type Props = {
 
 export function PostCard({ post }: Props) {
 
-  const likes = post.likes ?? 0;
-
   return (
     <div className="relative bg-white rounded-2xl p-8 shadow-sm max-w-prose mx-auto">
       <TypingPostText text={post.content} sound/>
